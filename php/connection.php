1ecf9f374d0ae8699594040ce240527f18092277
root <root@Slazzical.ngwwc4ak1psefbyij5aopyvhih.jnbx.internal.cloudapp.net>
2024-10-02 22:10:23 +0000

    Updating database connection yet again

--- php/connection.php
+++ php/connection.php
@@ -1,13 +1,13 @@
 <?php
-$host = "102.37.19.54";
+$servername = "localhost";
 $user = "root";
 $pass = "Hauntedgrounds5421"; // edit if you have set a password
 $name = "CAT1";
 
-$conn = new mysqli($host, $user, $pass, $name);
+$conn = new mysqli($servername, $user, $pass, $name);
 
 if($conn == TRUE){
-// 	echo "Connection succesful";
+ 	echo "Connection succesful";
 }
 
 else if ($conn->connect_error) {
